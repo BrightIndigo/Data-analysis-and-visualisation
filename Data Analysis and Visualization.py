5b094ecfa6928df53ec0d042c2e3d761a01ae424
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2024-01-03 15:46:00 +0100

    Add files via upload

--- Data Analysis and Visualization.py
+++ Data Analysis and Visualization.py
@@ -1,2 +1,16 @@
 import numpy as np 
+import csv
+import math
 
+with open('housing_price_dataset.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=",")
+    for row in reader:
+        price = row[0]
+        price = int(price)
+        data = list(price)
+        data_array = np.array(price)
+        data_array = np.array(price, dtype=float)
+        mean = np.mean(data_array)
+        max_value = np.max(data_array)
+
+print("Data array", data_array)
