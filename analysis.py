07cc816cbb35b7db5f30214aab2dede4092fd7d6
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2024-01-23 18:13:23 +0100

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 EmployeeID = []
 FirstName = []
@@ -18,7 +20,27 @@ with open("data.csv", newline='') as f:
         Department.append(row[4])
         Salary.append(int(row[5]))  # Convert salary to integer
 
-# Example: Find the average salary
-average_salary = sum(Salary) / len(Salary)
+EmployeeID_arr = np.array(EmployeeID)
+FirstName_arr = np.array(FirstName)
+LastName_arr = np.array(LastName)
+Age_arr = np.array(Age)
+Department_arr = np.array(Department)
+Salary_arr = np.array(Salary)
 
-print(f"Average Salary: {average_salary}")
+mean_salary = np.mean(Salary_arr)
+median_salary = np.median(Salary_arr)
+std_dev_salary = np.std(Salary_arr)
+#=============================================
+
+fig, ax = plt.subplots()
+
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(Department_arr, Salary_arr, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.show()
